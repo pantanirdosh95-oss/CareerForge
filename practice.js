97ce2d08d5ce92fa6c03f38e47f9e08e8792989f
Nirdosh Panta <pantanirdosh95@gmail.com>
2026-09-24 22:59:01 -0400

    Practice JavaScript destructuring spread and template literals

--- practice.js
+++ practice.js
@@ -1,22 +1,5 @@
-const cars = [
-    {
-        make: "Toyota",
-        model: "Supra",
-        horsepower: 400
-    },
-    {
-        make: "Honda",
-        model: "Type R",
-        horsepower: 300
-    },
-    {
-        make: "Ford",
-        model: "Mustang",
-        horsepower: 350
-    }
-];
+const brand = "Toyota";
+const model = "Supra";
+const horsepower = 382;
 
-const powerfulCars = cars.filter((car) => {
-    return car.horsepower >= 350;
-});
-console.log(powerfulCars);
+console.log(`${brand} ${model} has ${horsepower} horsepower.`);
